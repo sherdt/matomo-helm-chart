67d730eab60e23f1df836c98d2d72655c2ede5ff
insekticid <insekticid@exploit.cz>
2018-10-04 16:14:53 +0200

    PHP 7.2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,10 @@ if [ ! -e piwik.php ]; then
 	chown -R www-data .
 fi
 
+sed -i -e "s/pm.max_children = 5/pm.max_children = 200/g" $PHP_INI_DIR/../php-fpm.d/www.conf && \
+sed -i -e "s/pm.start_servers = 2/pm.start_servers = 3/g" $PHP_INI_DIR/../php-fpm.d/www.conf && \
+sed -i -e "s/pm.min_spare_servers = 1/pm.min_spare_servers = 2/g" $PHP_INI_DIR/../php-fpm.d/www.conf && \
+sed -i -e "s/pm.max_spare_servers = 3/pm.max_spare_servers = 10/g" $PHP_INI_DIR/../php-fpm.d/www.conf && \
+sed -i -e "s/pm.max_requests = 500/pm.max_requests = 1000/g" $PHP_INI_DIR/../php-fpm.d/www.conf
+
 exec "$@"
